e25672a032ee748179a7768d7daeb489e7a47f4b
Denis S. Badanin <otd105@npcap.ru>
2022-03-10 19:23:22 +0300

    wip

--- stuff/hash_chk.py
+++ stuff/hash_chk.py
@@ -1,6 +1,55 @@
 #!/usr/bin/python3
+import zlib
+# clothes00_EE,
+# clothes00_nml_EE,
+# skin00_EE,
+# skin00_nml_EE
+# clothes00_outline_EE,
+# cloth_fragment
+
+dds_name = 'clothes00_outline_EE'
+
+known_initial = {
+    2:  0x65e3626d, # 00 02
+    # no 3, 4 
+    5:  0x0b840636, # obj12 obj13 obj44
+    6:  0x50b362ef, # 00_spc 01_spc obj115
+    7:  0xb979a551, # csl_nml non_add non_spc
+    8:  0x101b2f58, # csl_body break_RR
+    9:  0xce3f7ab5, # skin00_EE obj44_spc obj13_spc
+    10: 0x51f4535b, # skirt00_EE
+    11: 0x25391b1c, # height00_EE ef_scroll10
+    12: 0x59a3c798, # wetbura00_EE bura00_00_EE clothes00_EE
+    13: 0x1f90f357, # skin00_nml_EE pants00_00_EE
+    14: 0x451a44fb, # skirt00_nml_EE
+    15: 0xca72f004, # skirt00_brk0_EE skirt00_brk1_EE swim_blend00_EE
+    16: 0xc758f1c7, # bura00_00_nml_EE clothes00_nml_EE clothes00_wet_EE pl11_weapon02_EE
+    17: 0x8033134c, # acce_body03_00_EE clothes00_brk0_EE clothes00_brk1_EE pants00_00_nml_EE
+    18: 0x90a7ddc8, # bg05_yuka_minasoko
+    #no 19
+    20: 0xd9cf5811, # pl11_weapon02_nml_EE
+    21: 0x1668a9da, # richacce_head08_00_EE  acce_body03_00_nml_EE clothes00_brk1_wet_EE 
+    22: 0x3eb8768a, # bg05_yuka_minasoko_spc
+    #no 23, 24
+    25: 0x5448a65f, # richacce_head08_00_nml_EE 
+}
+print("image name:", dds_name)
+dl = len(dds_name)
+print("Img name len:", dl)
+seed = known_initial[dl]
+print("Img seed:", hex(seed))
+res = zlib.crc32(bytearray(dds_name.encode('ascii')), seed ) 
+print("Your hash:", hex(res))
+
+
+
+
+
+
+exit()
+
 import sys
-a = bytearray('obj'.encode('ascii'))
+a = bytearray('zako01_00_EE'.encode('ascii'))
 print(a)
 crc = 0x04C11DB7
 for x in a:
